445894430297ecc17a6f46013895423a615ead26
chrisgoodell <cg53142@fastmail.com>
2018-05-04 09:45:13 -0400

    Update db column names

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const signature = '@!3$%%^&1ed^&*!l@#^&***()R0441';
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
 
 //db queries
 let createUser = (user) =>
@@ -23,26 +22,29 @@ let getMyCookBooksFromDB = (id) =>
   db.query(`SELECT * from cookbooks WHERE id = ${id};`)
 
 let searchTerms = (req, res) =>
-  db.query(`SELECT title from recipes;`)
+  db.query(`SELECT title, id from recipes;`)
   .then(terms => res.send(terms))
   .catch(err => res.send(err))
 
-let postRecipeToDB = (recipe) =>
-  db.query(`INSERT INTO "public"."recipes"("title", "ver", "prepmins", "cookmins",
-  "descr", "user_id", "ingredients", "directions", "servings")
-  VALUES('${recipe.title}', ${recipe.ver}, ${recipe.prepmins}, ${recipe.cookmins}, '${recipe.descr}', ${recipe.user_id},
-  '${recipe.ingredients}', '${recipe.directions}', ${recipe.servings})
-  RETURNING "id", "title", "ver", "derived_id", "prepmins", "cookmins", "createdon",
-  "descr", "tag", "user_id", "ingredients", "directions", "servings", "image_url";`)
+let postRecipeToDB = (recipe, userId) => {
+  console.log(recipe.title, recipe.ver, recipe.prepmins, recipe.cookmins, recipe.descr, userId )
+  return (
+    db.query(`INSERT INTO "public"."recipes"("title", "version", "prepmins", "cookmins",
+    "description", "user_id", "ingredients", "directions", "servings", "derived_id", "image_url", tag)
+    VALUES('${recipe.title}',${recipe.ver}, ${recipe.prepmins}, ${recipe.cookmins}, '${recipe.descr}', ${userId},
+    '${recipe.ingredients}', '${recipe.directions}', ${recipe.servings}, '$1', 'google.com', 'none')
+    RETURNING "id", "title", "version", "derived_id", "prepmins", "cookmins",
+    "description", "tag", "user_id", "ingredients", "directions", "servings", "image_url";`)
+  )
+}
 
-//new recipe  
+//new recipe
 let postNewRecipeToDB = (recipe) =>
-  db.query(`INSERT INTO "public"."recipes"("title", "ver", "prepmins", "cookmins",
-  "descr", "user_id", "ingredients", "directions", "servings")
-  VALUES('${recipe.title}', '1', ${recipe.prepmins}, ${recipe.cookmins}, '${recipe.descr}', ${recipe.user_id},
-  '${recipe.ingredients}', '${recipe.directions}', ${recipe.servings})
-  RETURNING "id", "title", "ver", "derived_id", "prepmins", "cookmins", "createdon",
-  "descr", "tag", "user_id", "ingredients", "directions", "servings", "image_url";`)
+  db.query(`INSERT INTO "public"."recipes"("title", "version", "derived_id",
+  "prepmins", "cookmins", "description", "user_id", "directions", "servings",
+  "image_url") VALUES('Robby''s Guac', 1, '$1', 20, 20, 'desc', 1, 'dadauhduah', 2, 'hdaihdah')
+  RETURNING "id", "title", "version", "derived_id", "prepmins", "cookmins", "description", "tag", "user_id", "directions", "servings", "image_url", "categories_id", "ingredients", "notes";
+`)
 
 //post new recipe from old recipes
 let postForkedRecipeToDB = (recipe) =>
@@ -63,13 +65,25 @@ let createCookBookInDB = (cookbook) => {
   db.query(`INSERT into "public"."cookbooks"("user_id", "recipe_id") VALUES(${cookbook.user_id}, ${cookbook_recipe_id};`);
 }
 
+let getRecipeFromDB = (id) =>
+  db.query(`SELECT * from recipes where id = ${id};`)
 
-let getRecipeFromDB = (id) => {
-  console.log(id);
-  return db.query(`SELECT * from recipes where id = ${id};`)
+let getAllCategories = (req, res) =>
+  db.query(`SELECT * from categories`)
+    .then(categories => res.send(categories))
 
+let getAllIngredients = (req, res) =>
+  db.query(`SELECT * from ingredients`)
+    .then(ingredients => res.send(ingredients))
+
+let getSearchedRecipes = (queryString) => {
+  let queryBuilder = `SELECT * from recipes WHERE id IN (`
+  queryString.forEach(number => queryBuilder = queryBuilder + number + ',');
+  queryBuilder = queryBuilder.slice(0, -1);
+  queryBuilder += ');';
+  console.log(queryBuilder)
+  return db.query(queryBuilder)
 }
-
 
 //authorization
 let createToken = (userId) => {
@@ -129,13 +143,13 @@ let getMyCookBooks = (req, res) => {
 
 let postRecipe = (req, res) => {
   let recipe = req.body
+  console.log('recipe: ', recipe);
   let token = req.headers.authorization;
   let validation = tokenValidator(token);
-  return (
-    postRecipeToDB(recipe)
+  validation &&
+    postRecipeToDB(recipe, validation.userId)
     .then(response => res.send(response))
     .catch(err => res.send(err))
-  )
 }
 
 let postCookBook = (req, res) => {
@@ -157,11 +171,19 @@ let getRecipeByID = (req, res) => {
   .catch(err => res.send(err))
 }
 
+let getIdsFromLibrary = (req, res) => {
+  let queryString = [];
+  let library = req.body.searchLibrary;
+  library.map(item => queryString.push(item.id))
+  getSearchedRecipes(queryString)
+  .then(response => res.send(response))
+  .catch(err => res.send(err))
+}
+
 //Middleware
 app.use(bodyParser.json());
-app.get('/', function(req, res) {
-  res.send("Welcome to NodeJS app on Heroku!");
-});
+app.get('/all-categories', getAllCategories)
+app.get('/all-ingredients', getAllIngredients)
 app.get('/recipes', getMyRecipes)
 app.post('/recipes', postRecipe)
 app.get('/all-recipes', getAllRecipes)
@@ -171,6 +193,7 @@ app.post('/users', postUser)
 app.post('/signin', signIn)
 app.get('/recipe', getRecipeByID)
 app.get('/search', searchTerms)
+app.post('/search-recipes', getIdsFromLibrary)
 
 
-app.listen(port, () => console.log('Recipes running on 3000'));
+app.listen(3000, () => console.log('Recipes running on 3000'))
